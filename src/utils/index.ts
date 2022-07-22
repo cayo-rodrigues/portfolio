bb76d4f0d1a0eaa251348de5b38a4bc37caa8168
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-07-22 13:10:29 -0300

    feat: include a new project and more techs

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,21 +1,25 @@
-import pwImg from "../assets/projectLogos/pw.png"
-import yocaImg from "../assets/projectLogos/yoca.png"
+import pwImg from "../assets/projectLogos/pw.png";
+import yocaImg from "../assets/projectLogos/yoca.png";
+import primeiraFilaImg from "../assets/projectLogos/pf.png";
 
-import pyImg from "../assets/techLogos/py.png"
-import jsImg from "../assets/techLogos/js.png"
-import tsImg from "../assets/techLogos/ts.png"
-import reactImg from "../assets/techLogos/react.png"
-import nodeImg from "../assets/techLogos/node.png"
-import expressImg from "../assets/techLogos/express.svg"
-import typeormImg from "../assets/techLogos/typeorm.png"
-import psqlImg from "../assets/techLogos/psql.png"
-import sqliteImg from "../assets/techLogos/sqlite.png"
-import djImg from "../assets/techLogos/dj.png"
-import htmlImg from "../assets/techLogos/html.png"
-import cssImg from "../assets/techLogos/css.png"
+import pyImg from "../assets/techLogos/py.png";
+import jsImg from "../assets/techLogos/js.png";
+import tsImg from "../assets/techLogos/ts.png";
+import reactImg from "../assets/techLogos/react.png";
+import nodeImg from "../assets/techLogos/node.png";
+import expressImg from "../assets/techLogos/express.svg";
+import typeormImg from "../assets/techLogos/typeorm.png";
+import psqlImg from "../assets/techLogos/psql.png";
+import sqliteImg from "../assets/techLogos/sqlite.png";
+import djImg from "../assets/techLogos/dj.png";
+import htmlImg from "../assets/techLogos/html.png";
+import cssImg from "../assets/techLogos/css.png";
+import s3Img from "../assets/techLogos/s3.png";
+import dockerImg from "../assets/techLogos/docker.png";
+import drfImg from "../assets/techLogos/drf.png";
 
-import { IProject } from "../interfaces/projects"
-import { ITech } from "../interfaces/techs"
+import { IProject } from "../interfaces/projects";
+import { ITech } from "../interfaces/techs";
 
 export const projectList: IProject[] = [
   {
@@ -34,7 +38,15 @@ export const projectList: IProject[] = [
     projectLink: "https://cayoca.herokuapp.com/api/docs/",
     repoLink: "https://github.com/cayo-rodrigues/yoca",
   },
-]
+  {
+    title: "Primeira Fila",
+    description:
+      "Uma aplicação backend que permite um usuário ver quais filmes estão em cartaz e onde eles estão sendo exibidos, além de comprar ingressos online. Ao mesmo tempo, gerentes de cinema podem cadastrar seus cinemas e gerenciá-los.",
+    logoSrc: primeiraFilaImg,
+    projectLink: "http://primeira-fila.herokuapp.com/docs/",
+    repoLink: "https://github.com/cayo-rodrigues/primeira-fila",
+  },
+];
 
 export const techList: ITech[] = [
   {
@@ -86,4 +98,12 @@ export const techList: ITech[] = [
     logoSrc: cssImg,
     name: "CSS",
   },
-]
+  {
+    logoSrc: s3Img,
+    name: "Amazon S3",
+  },
+  {
+    logoSrc: dockerImg,
+    name: "Docker",
+  },
+];
