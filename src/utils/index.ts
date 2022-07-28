c0cc8494b8260579d16d44a68886ee8dc02db919
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-07-28 12:48:16 -0300

    refact: tech logos src

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,26 +1,42 @@
+import { IProject } from "../interfaces/projects";
+import { ITech } from "../interfaces/techs";
+
+// project logos
 import pwImg from "../assets/projectLogos/pw.png";
 import yocaImg from "../assets/projectLogos/yoca.png";
 import primeiraFilaImg from "../assets/projectLogos/pf.png";
 import bolinhaImg from "../assets/projectLogos/bolinha.png";
 import joKingImg from "../assets/projectLogos/jk.png";
 
-import pyImg from "../assets/techLogos/py.png";
-import jsImg from "../assets/techLogos/js.png";
-import tsImg from "../assets/techLogos/ts.png";
-import reactImg from "../assets/techLogos/react.png";
-import nodeImg from "../assets/techLogos/node.png";
+// local tech logos
 import expressImg from "../assets/techLogos/express.svg";
 import typeormImg from "../assets/techLogos/typeorm.png";
-import psqlImg from "../assets/techLogos/psql.png";
 import sqliteImg from "../assets/techLogos/sqlite.png";
 import djImg from "../assets/techLogos/dj.png";
-import htmlImg from "../assets/techLogos/html.png";
-import cssImg from "../assets/techLogos/css.png";
-import s3Img from "../assets/techLogos/s3.png";
-import dockerImg from "../assets/techLogos/docker.png";
 
-import { IProject } from "../interfaces/projects";
-import { ITech } from "../interfaces/techs";
+// remote tech logos
+const dockerImg =
+  "https://camo.githubusercontent.com/cbd55750b53c01dc18830d377c7364b01077e8a675a79d454a3f1ea549efe129/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f646f636b65722f646f636b65722d6f726967696e616c2e737667";
+const jsImg =
+  "https://camo.githubusercontent.com/528e232c728b497080cbf31d2a7e797caa81e402ff81643f79b2c2c395a29f17/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6a6176617363726970742f6a6176617363726970742d706c61696e2e737667";
+const cssImg =
+  "https://camo.githubusercontent.com/2e496d4bfc6f753ddca87b521ce95c88219f77800212ffa6d4401ad368c82170/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f637373332f637373332d6f726967696e616c2e737667";
+const htmlImg =
+  "https://camo.githubusercontent.com/da7acacadecf91d6dc02efcd2be086bb6d78ddff19a1b7a0ab2755a6fda8b1e9/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f68746d6c352f68746d6c352d6f726967696e616c2e737667";
+const reactImg =
+  "https://camo.githubusercontent.com/27d0b117da00485c56d69aef0fa310a3f8a07abecc8aa15fa38c8b78526c60ac/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f72656163742f72656163742d6f726967696e616c2e737667";
+const psqlImg =
+  "https://camo.githubusercontent.com/d536b9cc0c533324368535ece721f5424f28eae3ec0e6f3847408948ecacfce6/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f706f737467726573716c2f706f737467726573716c2d6f726967696e616c2e737667";
+const tsImg =
+  "https://camo.githubusercontent.com/c04208976fe84f5bfd2111ba446acf65ff373c962ed80bbf7aa028820a5bbd79/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f747970657363726970742f747970657363726970742d706c61696e2e737667";
+const pyImg =
+  "https://camo.githubusercontent.com/dd8b0601cdfefe534a6a26f4c29c7f8a5fcfc315002655f519c73121f7bad8bc/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f707974686f6e2f707974686f6e2d6f726967696e616c2e737667";
+const nodeImg =
+  "https://camo.githubusercontent.com/900baefb89e187c8b32cdbb3b440d1502fe8f30a1a335cc5dc5868af0142f8b1/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6e6f64656a732f6e6f64656a732d6f726967696e616c2e737667";
+const awsImg =
+  "https://camo.githubusercontent.com/9b70a59580f9cdb57e2083a8085cfb55614d3c145cc56ac8a3d3b81870d0f3d1/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f616d617a6f6e77656273657276696365732f616d617a6f6e77656273657276696365732d6f726967696e616c2e737667";
+const ubuntuImg =
+  "https://camo.githubusercontent.com/3e8e4170de9ac8fc3a7bbe397f78556429a19e26dbefe7020d1e8c25c6ace93c/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f7562756e74752f7562756e74752d706c61696e2e737667";
 
 export const projectList: IProject[] = [
   {
@@ -116,11 +132,15 @@ export const techList: ITech[] = [
     name: "CSS",
   },
   {
-    logoSrc: s3Img,
+    logoSrc: awsImg,
     name: "Amazon S3",
   },
   {
     logoSrc: dockerImg,
     name: "Docker",
+  },
+  {
+    logoSrc: ubuntuImg,
+    name: "Ubuntu",
   },
 ];
