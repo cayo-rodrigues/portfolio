8beb83d3a37beec08d7acfc753c9a41ae1805ed6
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2023-02-16 21:41:03 -0300

    feat: new tech logos
    - typeorm, ubuntu and aws logos have been removed

--- src/utils/techLogos.ts
+++ src/utils/techLogos.ts
@@ -1,10 +1,10 @@
 // local tech logos
 import expressImg from "../assets/techLogos/express.svg"
-import typeormImg from "../assets/techLogos/typeorm.png"
 import sqliteImg from "../assets/techLogos/sqlite.png"
 import djImg from "../assets/techLogos/dj.png"
+import githubLogo from "../assets/techLogos/github.svg"
 
-export { expressImg, typeormImg, sqliteImg, djImg }
+export { expressImg, sqliteImg, djImg, githubLogo }
 
 // remote tech logos
 export const pyImg =
@@ -12,13 +12,15 @@ export const pyImg =
 export const reactImg =
   "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
 export const tsImg =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg"
 export const jsImg =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"
 export const nodeImg =
   "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
 export const psqlImg =
   "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg"
+export const mysqlImg =
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
 export const redisImg =
   "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redis/redis-original.svg"
 export const cImg =
@@ -27,11 +29,11 @@ export const htmlImg =
   "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
 export const cssImg =
   "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
-export const awsImg =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original.svg"
 export const dockerImg =
   "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
-export const ubuntuImg =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ubuntu/ubuntu-plain.svg"
+export const linuxImg =
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg"
 export const gitImg =
   "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+export const seleniumImg =
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/selenium/selenium-original.svg"
