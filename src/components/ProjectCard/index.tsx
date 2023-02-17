ab33b8be6bb406afa781aed4ee1488bb10ae1e31
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2023-02-17 08:57:03 -0300

    perf: cache project cards
    - so that images only load once

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,54 +1,51 @@
+import React from "react"
 import { useLang } from "../../contexts/LangContext"
 import { IProject } from "../../interfaces/projects"
 import LogoImg from "../LogoImg"
 import Title from "../Title"
 import { CardButtonsContainer, CardContainer } from "./styles"
 
-const ProjectCard = ({
-  description,
-  logoSrc,
-  projectLink,
-  repoLink,
-  title,
-}: IProject) => {
-  const { currentLang } = useLang()
+const ProjectCard = React.memo(
+  ({ description, logoSrc, projectLink, repoLink, title }: IProject) => {
+    const { currentLang } = useLang()
 
-  const buttonsText = {
-    project: {
-      pt: "Ver Projeto",
-      en: "See Project",
-    },
-    repo: {
-      pt: "Código Fonte",
-      en: "Source Code",
-    },
+    const buttonsText = {
+      project: {
+        pt: "Ver Projeto",
+        en: "See Project",
+      },
+      repo: {
+        pt: "Código Fonte",
+        en: "Source Code",
+      },
+    }
+
+    return (
+      <CardContainer>
+        <LogoImg
+          src={logoSrc}
+          alt="Proworking logo"
+          width="30%"
+          widthMobile="36%"
+        />
+
+        <Title tag={"h4"} size={"1.2rem"}>
+          {title}
+        </Title>
+
+        <p>{description[currentLang]}</p>
+
+        <CardButtonsContainer>
+          <a href={projectLink} target="_blank" rel="noopener noreferrer">
+            <button>{buttonsText.project[currentLang]}</button>
+          </a>
+          <a href={repoLink} target="_blank" rel="noopener noreferrer">
+            <button>{buttonsText.repo[currentLang]}</button>
+          </a>
+        </CardButtonsContainer>
+      </CardContainer>
+    )
   }
-
-  return (
-    <CardContainer>
-      <LogoImg
-        src={logoSrc}
-        alt="Proworking logo"
-        width="30%"
-        widthMobile="36%"
-      />
-
-      <Title tag={"h4"} size={"1.2rem"}>
-        {title}
-      </Title>
-
-      <p>{description[currentLang]}</p>
-
-      <CardButtonsContainer>
-        <a href={projectLink} target="_blank" rel="noopener noreferrer">
-          <button>{buttonsText.project[currentLang]}</button>
-        </a>
-        <a href={repoLink} target="_blank" rel="noopener noreferrer">
-          <button>{buttonsText.repo[currentLang]}</button>
-        </a>
-      </CardButtonsContainer>
-    </CardContainer>
-  )
-}
+)
 
 export default ProjectCard
