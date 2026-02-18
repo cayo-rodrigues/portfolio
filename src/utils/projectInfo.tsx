2160d2c83df92db6d333cc6e9bc11cf6e9c58438
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2026-02-18 17:12:23 -0300

    fix: remove outdated projects

--- src/utils/projectInfo.tsx
+++ src/utils/projectInfo.tsx
@@ -58,36 +58,6 @@ export const projectList: IProject[] = [
     repoLink: "https://github.com/cayo-rodrigues/gsic",
   },
   {
-    title: "Primeira Fila",
-    description: {
-      pt: "Uma aplicação backend que permite um usuário ver quais filmes estão em cartaz e onde eles estão sendo exibidos, além de comprar ingressos online. Ao mesmo tempo, gerentes de cinema podem cadastrar seus cinemas e gerenciá-los.",
-      en: "A backend app, which allows users to look for movies, see where each one are playing, the prices in each place and buy tickets online. Also, cinema managers can register their cinemas and manage their movie sessions.",
-    },
-    logoSrc: PL.primeiraFilaImg,
-    projectLink: "https://primeira-fila-api.up.railway.app/docs/",
-    repoLink: "https://github.com/cayo-rodrigues/primeira-fila",
-  },
-  {
-    title: "Yoca",
-    description: {
-      pt: "Uma api com o objetivo de gerenciar os processos envolvidos na administração de um restaurante, com controle de estoque semi-automático.",
-      en: "An api whose purpose in life is to manage many proccess related to a restaraut's workflow, having a semi-automatic inventory control system.",
-    },
-    logoSrc: PL.yocaImg,
-    projectLink: "https://cayoca.up.railway.app/api/docs/",
-    repoLink: "https://github.com/cayo-rodrigues/yoca",
-  },
-  {
-    title: "Proworking",
-    description: {
-      pt: "Um projeto cujo objetivo é conectar de forma fácil e simples prestadores de serviço informais com pessoas que precisam desses serviços.",
-      en: "The goal of this project is to connect informal workers with people who need some kind of work done, in a simple and easy way",
-    },
-    logoSrc: PL.pwImg,
-    projectLink: "https://proworking.vercel.app/",
-    repoLink: "https://github.com/cayo-rodrigues/capstone-m3",
-  },
-  {
     title: "Jogo da bolinha",
     description: {
       pt: "Um joguinho simples cujo objetivo é não deixar a bola cair. Conforme o tempo passa o jogador acumula pontos, e a bolinha fica mais rápida.",
